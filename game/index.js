0716c013567557a5f774234b9a5dda2bdad147a9
Soheila Bayat <soheila.bayat1990@gmail.com>
2018-07-05 12:19:19 -0400

    first

--- game/index.js
+++ game/index.js
@@ -1,4 +1,24 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable'
+import { createStore } from 'redux'
+
+let board = Map()
+board = board.setIn([1, 1], 'X')
+const move = function(arr, ){
+
+}
+const reducer= (state = initialState, action) => {
+  switch(action.type) {
+    case 'move':
+    return {board.setIn(arr)}
+  }
+}
+
+const store = createStore(reducer)
+store.dispatch({type, player, position
+})
+
+
+
 
 export default function reducer(state, action) {
   // TODO
